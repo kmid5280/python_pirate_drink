9e36a9dd5b307256ba816f4a277640e7dd075cdd
Kyle Middleton <kmiddleton654@gmail.com>
2018-10-19 14:30:39 -0600

    replaced .append with .extend in return_ingredients function. removed test code and fixed typo

--- python_pirate_drink.py
+++ python_pirate_drink.py
@@ -10,7 +10,7 @@ ingredients = {
   "strong": ["glug of rum", "slug of whisky", "splash of gin"],
   "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
   "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
-  "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+  "sweet": ["sugar cube", "spoonful of honey", "splash of cola"],
   "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
 }
 
@@ -29,12 +29,10 @@ def pirate_drink():
     strong = input(questions["strong"])
     if strong == 'y' or strong == 'yes':
       responses['strong'] = True
-      print(responses)
-      print(ingredients["strong"])
+      print("Adding strong ingredients then.")
     elif strong == 'n' or strong == 'no':
       responses['strong'] = False
       print('No strong then.')
-      print(responses)
     else:
       print("y or n")
       
@@ -42,7 +40,7 @@ def pirate_drink():
     salty = input(questions["salty"])
     if salty == 'y' or salty == 'yes':
       responses['salty'] = True
-      print(ingredients["salty"])
+      print("Adding salty ingredients then.")
     elif salty == 'n' or salty == 'no':
       responses['salty'] = False
       print('No salty then.')
@@ -53,7 +51,7 @@ def pirate_drink():
     bitter = input(questions["bitter"])
     if bitter == 'y':
       responses['bitter'] = True
-      print(ingredients["bitter"])
+      print("Adding bitter ingredients then.")
     elif bitter == 'n':
       responses['bitter'] = False
       print('No bitter then.')
@@ -64,7 +62,7 @@ def pirate_drink():
     sweet = input(questions["sweet"])
     if sweet == 'y':
       responses['sweet'] = True
-      print(ingredients["sweet"])
+      print("Adding sweet ingredients then.")
     elif sweet == 'n':
       responses['sweet'] = False
       print('No sweet then.')
@@ -75,7 +73,7 @@ def pirate_drink():
     fruity = input(questions["fruity"])
     if fruity == 'y':
       responses['fruity'] = True
-      print(ingredients["fruity"])
+      print("Adding fruity ingredients then.")
     elif fruity == 'n':
       responses['fruity'] = False
       print('No fruity then.')
@@ -86,15 +84,15 @@ def pirate_drink():
 def return_ingredients():
   """This function adds necessary ingredients to the drink based on the user's responses."""
   if responses['strong'] == True:
-    ingredient_results.append(ingredients['strong'])
+    ingredient_results.extend(ingredients['strong'])
   if responses['salty'] == True:
-    ingredient_results.append(ingredients['salty'])
+    ingredient_results.extend(ingredients['salty'])
   if responses['bitter'] == True:
-    ingredient_results.append(ingredients['bitter'])
+    ingredient_results.extend(ingredients['bitter'])
   if responses['sweet'] == True:
-    ingredient_results.append(ingredients['sweet'])
+    ingredient_results.extend(ingredients['sweet'])
   if responses['fruity'] == True:
-    ingredient_results.append(ingredients['fruity'])
+    ingredient_results.extend(ingredients['fruity'])
   
   print("Your total ingredients are: ", ingredient_results)
 
