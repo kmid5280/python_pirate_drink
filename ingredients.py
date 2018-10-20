ca0c5f35d068c7aa4cf8cf2f4af093b8ecd5577f
Kyle Middleton <kmiddleton654@gmail.com>
2018-10-19 19:23:08 -0600

    moved questions list and ingredients list to separate files

---
+++ ingredients.py
@@ -0,0 +1,7 @@
+ingredients = {
+  "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+  "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+  "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+  "sweet": ["sugar cube", "spoonful of honey", "splash of cola"],
+  "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+}
---
+++ questions.py
@@ -0,0 +1,7 @@
+questions = {
+  "strong": "Do ye like yer drinks strong?",
+  "salty": "Do ye like it with a salty tang?",
+  "bitter": "Are ye a lubber who likes it bitter?",
+  "sweet": "Would ye like a bit of sweetness with yer poison?",
+  "fruity": "Are ye one for a fruity finish?",
+}
